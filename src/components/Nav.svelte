8ff09e69236498772c6b699c9b169a172c0579db
codingwith3dv <deveshvdddas@gmail.com>
2022-01-09 18:44:49 +0500

    :tada: Initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,3 @@
+<div>
+  <h1 class="text-zinc-100">CGQL</h1>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "../app.css";
+  import Nav from "../components/Nav.svelte"
+</script>
+<main>
+  <Nav/>
+  <h1 class="text-sky-400">Hey</h1>
+</main>
